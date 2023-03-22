710a889cdbc9748a41910ecb67df3a915044e973
apoor <apoor@hp>
2023-03-22 13:23:10 +0530

    Pattern programs

---
+++ src/Javaprograms/Program1.java
@@ -0,0 +1,18 @@
+package Javaprograms;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+
+				for(int i=1;i<=5;i++) {
+					
+				
+				// TODO Auto-generated method stub
+				System.out.println("Hello Java World");
+			}
+
+		}
+		}
+	
+
+
---
+++ src/Javaprograms/Program3.java
@@ -0,0 +1,16 @@
+package Javaprograms;
+
+public class Program3 {
+
+		public static void main(String[] args) {
+			int i=1;
+			do
+			{
+				System.out.println("Hello good eve");
+				i++;
+			}
+			while(i<=5);
+		}
+	}
+
+
